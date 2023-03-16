07cd63fcf5eb7f26314dce9587b5c9a5321d5caf
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-03-15 21:53:08 -0400

    debuged

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,51 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicle")
 
+class Car extends VehicleModule.Vehicle {
+    constructor(make, model, year, color, mileage,maximumPassengers, pasager, numberOfWheels,maximumSpeed, fuel, scheduleService){
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = maximumPassengers;
+        this.passenger = pasager;
+        this.numberOfWheels = numberOfWheels;
+        this.maximumSpeed = maximumSpeed;
+        this.fuel = fuel;
+        this.scheduleService = scheduleService;
+    }
+    loadPassenger(num){
+        if (this.passenger < this.maxPassengers) {
+         if((num + `${this.passenger}`) <= `${this.maximumPassengers}`)
+          this.passenger = num;
+          return this.passenger;
+       }else{
+         console.log(`${this.model} is full`);
+       }
+
+    }
+     start() {
+       if(this.fuel > 0){
+       console.log("Car has started");
+       return this.started = true;
+     }else{
+      console.log("no fuel");
+      return this.started = false;
+     }
+     }
+    
+    newService(){
+      if(this.mileage > 30000){
+          this.scheduleService = true;
+          console.log("service needed")
+        return this.scheduleService;
+        } 
+}
+}
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+ let mycar = new Car("Mecury", "Sedan", "1965", "Black", 40010, 5, 0, 4, 160, 10 );
+ mycar.start();
+ mycar.loadPassenger(0);
+ mycar.stop();
+ mycar.newService();
+ console.log(mycar)
+ const mycar1 = new Car("audi")
+ console.log(mycar1)
+
--- vehicle.js
+++ vehicle.js
@@ -1,5 +1,6 @@
 class Vehicle {
     constructor(make, model, year, color, mileage) {
+     
         this.make = make;
         this.model = model;
         this.year = year;
@@ -9,17 +10,9 @@ class Vehicle {
         this.mileage = mileage;
         this.started = false;
         this.numberOfWheels = 0;
+        this.fuel = 100
     }
 
-    start() {
-        if (this.fuel > 0) {
-            return this.started = true;
-            console.log("engine started...!!!");
-        } else {
-            return this.started = false;
-            console.log("engine cannot start...");
-        }
-    }
     accelerate() {
         if (this.started) {
             if (this.fuel > 0) {
@@ -33,6 +26,7 @@ class Vehicle {
             alert("You need to start the engine first.");
         }
     }
+
     decelerate() {
         if (this.started) {
             if (this.fuel > 0) {
@@ -51,33 +45,18 @@ class Vehicle {
             alert("You need to start the engine first.");
         }
     }
+
     stop() {
+        console.log('engine off')
         this.started = false;
     }
 
-    drive() {
-        accelerate();
-    }
-    brake() {
-        decelerate();
-    }
-
-    autoPark()
-    {
-
-    }
-
-    autoDrive()
-    {
-      
-    }
-
-    typeOfVehicle(wheels) {
-        if (this.numberOfWheels == 8 && 8 == wheels) {
+    typeOfVehicle() {
+        if (this.numberOfWheels == 8) {
             console.log(this.model + " " + this.make + " is a Truck");
-        } else if (this.numberOfWheels == 4 && 4 == wheels) {
-            console.log(this.model + " " + this.make + " is a CAr");
-        } else if (this.numberOfWheels == 2 && 2 == wheels) {
+        } else if (this.numberOfWheels == 4) {
+            console.log(this.model + " " + this.make + " is a Car");
+        } else if (this.numberOfWheels == 2) {
             console.log(this.model + " " + this.make + " is a Bike");
         } else {
             console.log("Unknown type of vehicle");
@@ -86,6 +65,4 @@ class Vehicle {
 }
 
 //This exports things you want to use from this "module", more info in readme
-module.exports = {
-    Vehicle
-}
+module.exports = { Vehicle }
